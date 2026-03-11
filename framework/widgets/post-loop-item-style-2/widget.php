f2b2d727c94b9b707f646543ecceb45f93129c8c
hieuhuynh <hieuhuynh.bearsthemes@gmail.com>
2026-03-11 08:24:09 +0700

    update widget setting

--- framework/widgets/post-loop-item-style-2/widget.php
+++ framework/widgets/post-loop-item-style-2/widget.php
@@ -34,12 +34,57 @@ class Widget_PostLoopItemStyle2 extends Widget_Base
 		return ['somnia'];
 	}
 
+	private function get_supported_posts() {
+		$posts = get_posts([
+			'post_type'      => 'post',
+			'post_status'    => 'publish',
+			'posts_per_page' => -1,
+			'orderby'        => 'date',
+			'order'          => 'DESC',
+		]);
+
+		$options = [];
+		foreach ( $posts as $post ) {
+			$options[ $post->ID ] = $post->post_title;
+		}
+
+		return $options;
+	}
+
 	protected function register_layout_section_controls()
 	{
 		$this->start_controls_section(
 			'section_content',
 			[
 				'label' => __('Content', 'somnia'),
+			]
+		);
+
+		$this->add_control(
+			'enable_manual_post',
+			[
+				'label'        => esc_html__( 'Manual Post', 'somnia' ),
+				'type'         => Controls_Manager::SWITCHER,
+				'label_on'     => esc_html__( 'Enable', 'somnia' ),
+				'label_off'    => esc_html__( 'Auto', 'somnia' ),
+				'return_value' => 'yes',
+				'default'      => '',
+			]
+		);
+
+		$options = $this->get_supported_posts();
+		$this->add_control(
+			'post_id',
+			[
+				'label'       => esc_html__( 'Select Post', 'somnia' ),
+				'type'        => Controls_Manager::SELECT2,
+				'options'     => $options,
+				'multiple'    => false,
+				'label_block' => true,
+				'condition'   => [
+					'enable_manual_post' => 'yes',
+				],
+				'default'     => ! empty( $options ) ? array_key_first( $options ) : '',
 			]
 		);
 
@@ -355,12 +400,34 @@ class Widget_PostLoopItemStyle2 extends Widget_Base
 
 	protected function render()
 	{
-		$settings = $this->get_settings_for_display();
-?>
+		$settings      = $this->get_settings_for_display();
+		$template_args = [
+			'image-size' => $settings['thumbnail_size'],
+			'excerpt'    => $settings['show_excerpt'],
+		];
+		?>
 		<div class="bt-elwg-post-loop-item--style2">
-			<?php get_template_part('framework/templates/post', 'style', array('image-size' => $settings['thumbnail_size'], 'excerpt' => $settings['show_excerpt'])); ?>
+			<?php
+			if ( $settings['enable_manual_post'] === 'yes' && ! empty( $settings['post_id'] ) ) {
+				$post_id = (int) $settings['post_id'];
+				$query   = new \WP_Query([
+					'post_type'      => 'post',
+					'p'              => $post_id,
+					'posts_per_page' => 1,
+				]);
+				if ( $query->have_posts() ) {
+					while ( $query->have_posts() ) {
+						$query->the_post();
+						get_template_part( 'framework/templates/post', 'style', $template_args );
+					}
+					wp_reset_postdata();
+				}
+			} else {
+				get_template_part( 'framework/templates/post', 'style', $template_args );
+			}
+			?>
 		</div>
-<?php
+		<?php
 	}
 
 	protected function content_template() {}
